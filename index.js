8612a557d03eed2454673335e9b4809c3119047f
Renato Monroy <78289548+rdev32@users.noreply.github.com>
2024-02-18 09:40:08 -0500

    camera fix

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ window.addEventListener('load', () => {
     const containerWidth = container.offsetWidth
     const containerHeight = container.offsetHeight
 
-    const posX = (mouseX / containerWidth) * 30
-    const posY = (mouseY / containerHeight) * 30
+    const posX = (mouseX / containerWidth) * 100
+    const posY = (mouseY / containerHeight) * 100
 
     container.style.transition = 'background-position 0.5s ease'
     container.style.backgroundPosition = `${posX}% ${posY}%`
